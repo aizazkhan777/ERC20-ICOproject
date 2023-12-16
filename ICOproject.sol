4a6dc8e9b088ecacaa4f67d86db3c1ed42f14058
aizazkhan777 <34534131+aizazkhan777@users.noreply.github.com>
2023-12-16 12:56:51 +0500

    Update ICOproject.sol
    giving token to 2 out of 4 subscribers

--- ICOproject.sol
+++ ICOproject.sol
@@ -1,4 +1,4 @@
-//SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
@@ -17,22 +17,31 @@ contract ICO {
 
     uint public minInvestment;
     uint public maxInvestment;
+    
+    struct Subscription {
+        address addr;
+        bool eligibleForTokens;
+    }
+
+    Subscription[4] public subscriptionList;
+    uint public subscriberCount = 0;
+    uint public eligibleSubscriberCount = 0;
 
     constructor(address _tokenAddress) {
-        MAKToken = IERC20(0xFAB7a3E98C551d11dBcb82eE3aA53c3d3C6d2c8E);
+        MAKToken = IERC20(_tokenAddress);
         admin = msg.sender;
         deposit = payable(msg.sender);
 
-        saleStart = block.timestamp; // Sale starts at deployment
-        saleEnd = saleStart + 604800; // 1 week duration
+        saleStart = block.timestamp;
+        saleEnd = saleStart + 604800;
 
-        hardCap = 0.02 ether; 
+        hardCap = 0.02 ether;
         totalInvested = 0;
 
         minInvestment = 0.001 ether;
         maxInvestment = 0.01 ether;
 
-        tokenPrice = 0.001 ether; // Adjust as per your tokenomics
+        tokenPrice = 0.001 ether;
     }
 
     modifier onlyAdmin() {
@@ -46,6 +55,49 @@ contract ICO {
         _;
     }
 
+    function addSubscriber() public {
+        require(subscriberCount < 4, "Subscription list is full");
+        subscriptionList[subscriberCount] = Subscription(msg.sender, eligibleSubscriberCount < 2);
+        subscriberCount++;
+        if (eligibleSubscriberCount < 2) {
+            eligibleSubscriberCount++;
+        }
+    }
+
+    function calculateTokensToBuy() internal pure returns (uint256) {
+        return 1; // Each eligible subscriber receives 1 token
+    }
+
+    function distributeTokens() public onlyAdmin {
+        require(block.timestamp > saleEnd, "Sale not ended");
+        require(eligibleSubscriberCount >= 2, "At least 2 eligible subscribers required");
+
+        address[] memory selectedSubscribers = new address[](2);
+        uint[] memory selectedIndices = new uint[](2);
+
+        for (uint i = 0; i < 2; i++) {
+            uint randomIndex;
+            do {
+                randomIndex = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, i))) % subscriberCount;
+            } while (!subscriptionList[randomIndex].eligibleForTokens);
+
+            selectedSubscribers[i] = subscriptionList[randomIndex].addr;
+            selectedIndices[i] = randomIndex;
+
+            subscriptionList[randomIndex].eligibleForTokens = false; // Mark subscriber as not eligible for future distributions
+        }
+
+        for (uint i = 0; i < 2; i++) {
+            MAKToken.transfer(selectedSubscribers[i], calculateTokensToBuy()); // Transfer tokens to selected subscribers
+        }
+
+        for (uint i = 0; i < subscriberCount; i++) {
+            if (i != selectedIndices[0] && i != selectedIndices[1]) {
+                payable(subscriptionList[i].addr).transfer(minInvestment); // Refund non-selected subscribers
+            }
+        }
+    }
+    
     function setDepositAddress(address _deposit) public onlyAdmin {
         deposit = payable(_deposit);
     }
@@ -57,6 +109,11 @@ contract ICO {
 
         totalInvested += msg.value;
         MAKToken.transfer(msg.sender, tokensToBuy);
+    }
+
+    function depositTokens(uint256 _amount) public {
+        require(block.timestamp < saleStart, "Sale has started");
+        require(MAKToken.transferFrom(msg.sender, address(this), _amount), "Token transfer failed");
     }
 
     function withdrawFunds() public onlyAdmin {
@@ -72,5 +129,4 @@ contract ICO {
         }
         deposit.transfer(address(this).balance);
     }
-    //0x030eccAA52a3713AF8592c5acB52055297F0559b contract address
 }
